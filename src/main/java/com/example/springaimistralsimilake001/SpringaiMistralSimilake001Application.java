97db3d355a8e7cb4a4298f7969cccf8932fa39ec
Pritesh <tinumistry@gmail.com>
2024-10-28 01:13:47 +0530

    removed the keys

--- src/main/java/com/example/springaimistralsimilake001/SpringaiMistralSimilake001Application.java
+++ src/main/java/com/example/springaimistralsimilake001/SpringaiMistralSimilake001Application.java
@@ -1,9 +1,12 @@
 package com.example.springaimistralsimilake001;
 
+import org.similake.client.properties.SimilakeProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringaiMistralSimilake001Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springaimistralsimilake001/config/AppConfig.java
+++ src/main/java/com/example/springaimistralsimilake001/config/AppConfig.java
@@ -1,15 +1,29 @@
 package com.example.springaimistralsimilake001.config;
 
+import org.similake.client.properties.SimilakeProperties;
 import org.similake.client.store.SimilakeVectorStore;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    private Environment environment;
+
     @Bean
-    public SimilakeVectorStore vectorStore(EmbeddingModel embdeddingModel) {
-        return new SimilakeVectorStore(embdeddingModel);
+    public SimilakeVectorStore vectorStore(EmbeddingModel embdeddingModel,SimilakeProperties properties) {
+        return new SimilakeVectorStore(embdeddingModel,properties);
+    }
+
+    @Bean
+    public SimilakeProperties  initProperties() {
+     /*   SimilakeProperties properties = new SimilakeProperties();
+        properties.setHost(environment.getProperty("spring.ai.vectorstore.similake.host"));*/
+
+        return new SimilakeProperties();
     }
 }
